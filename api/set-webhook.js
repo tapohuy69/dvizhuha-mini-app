dd09d92ec96cde6040fbe0f6d7922982800da2ac
tapohuy69 <tapohuy69@gmail.com>
2026-08-12 05:17:51 +0200

    Update set-webhook.js

--- api/set-webhook.js
+++ api/set-webhook.js
@@ -1,1 +1,20 @@
+export default async function handler(req, res) {
+  const token = process.env.TELEGRAM_BOT_TOKEN;
 
+  if (!token) {
+    return res.status(500).json({
+      error: "TELEGRAM_BOT_TOKEN не найден"
+    });
+  }
+
+  const webhookUrl =
+    "https://dvizhuha-mini-lo4yssmjs-danya5.vercel.app/api/telegram";
+
+  const response = await fetch(
+    `https://api.telegram.org/bot${token}/setWebhook?url=${encodeURIComponent(webhookUrl)}`
+  );
+
+  const data = await response.json();
+
+  return res.status(response.status).json(data);
+}
